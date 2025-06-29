e356fe260274fda06d3d056663ec9b426f2e5f4b
Neamat Abdollah <engcomm2016@gmail.com>
2025-06-29 03:53:46 +0300

    Update some configuration in app.config.ts file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -6,6 +6,9 @@ import Aura from '@primeng/themes/aura';
 
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +19,9 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura
       }
-    })
+    }),
+    provideHttpClient(),
+    importProvidersFrom(BrowserAnimationsModule),
+    MessageService
   ]
 };
