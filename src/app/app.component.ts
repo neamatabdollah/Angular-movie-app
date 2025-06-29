6156910678fea8b7f285985cdf1a68088d2fd430
Neamat Abdollah <engcomm2016@gmail.com>
2025-06-29 05:04:55 +0300

    Routes configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent,ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test';
+  title = 'movie-app';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/movies',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    loadComponent: () => import('./components/movies-list/movies-list.component').then(m => m.MoviesListComponent)
+  },
+  {
+    path: 'movie/:id',
+    loadComponent: () => import('./components/movie-card/movie-card.component').then(m => m.MovieCardComponent)
+  },
+  {
+    path: 'tv-shows',
+    loadComponent: () => import('./components/tv-shows/tv-shows.component').then(m => m.TvShowsComponent)
+  },
+  {
+    path: 'tv/:id',
+    loadComponent: () => import('./components/tv-details/tv-details.component').then(m => m.TvDetailsComponent)
+  },
+  {
+    path: 'wishlist',
+    loadComponent: () => import('./components/wishlist/wishlist.component').then(m => m.WishlistComponent)
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./components/search/search.component').then(m => m.SearchComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent)
+  }
+];
