efb2de0659d96040c06cdb90b61de7b215d5b236
Neamat Abdollah <engcomm2016@gmail.com>
2025-06-29 04:03:38 +0300

    Create all components

---
+++ src/app/components/tv-details/tv-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-details',
+  imports: [],
+  templateUrl: './tv-details.component.html',
+  styleUrl: './tv-details.component.scss'
+})
+export class TvDetailsComponent {
+
+}
