e0e8fedf67fb476fd486fdf56e7d0d14c3d29c90
Neamat Abdollah <engcomm2016@gmail.com>
2025-07-01 04:32:11 +0300

    Adding the not-found page content

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [CommonModule, RouterModule, ButtonModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
